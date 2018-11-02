b843422cc90313842072884e085e7ede2466fe26
Manik Sharma <maniksharma98144@gmail.com>
2018-11-02 22:17:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var request = require('request');
+var bodyparser = require('body-parser');
+
+var app = express();
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json())
+app.listen(process.env.PORT || 5000);
+
+// Server index page
+app.get('/', (req, res) => {
+    res.send('Deployed!')
+});
+
+// Facebook Webhook
+// Used for verification
+app.get('/webhook', () => {
+    if (req.query["hub.verify_token"] === "this_is_my_token") {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
+})
